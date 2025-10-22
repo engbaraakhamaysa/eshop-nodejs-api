4d8fa60479410ce3bb53a887f2ddc62f1da7aeea
Baraa <engbaraakhamaysa@gmail.com>
2025-10-22 07:11:53 +0300

    Add Login Api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,4 +31,29 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+//Login
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await Client.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Incorrect email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect email or password" });
+    }
+
+    res
+      .status(200)
+      .json({ user: { _id: user._id, name: user.name, email: user.email } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports = { register, login };
