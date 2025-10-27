935414336c58af38901cce5d6204ac20dbdd5517
Baraa <engbaraakhamaysa@gmail.com>
2025-10-27 13:36:13 +0200

    Add Access Token & Refersh Token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,26 @@
 const bcrypt = require("bcryptjs");
 const Client = require("../models/Client.model");
+const jwt = require("jsonwebtoken");
 
+//Create JWT token
+const createAccessToken = (user) => {
+  return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+//Create Refresh Token
+const createRefershToken = (user) => {
+  return jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_REFRESH_SECRET,
+    {
+      expiresIn: process.env.JWT_REFRESH_EXPIRE,
+    }
+  );
+};
+
+//Register Contorller
 const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -18,12 +38,16 @@ const register = async (req, res) => {
 
     const newClient = new Client({ name, email, password: hashedPassword });
     await newClient.save();
+
+    const accessToken = createAccessToken(newClient);
+    const refershToken = createRefershToken(newClient);
     res.status(200).json({
       user: {
         _id: newClient._id,
         name: newClient.name,
         email: newClient.email,
       },
+      token: { Access: accessToken, Refersh: refershToken },
     });
   } catch (err) {
     console.error(err);
@@ -31,7 +55,7 @@ const register = async (req, res) => {
   }
 };
 
-//Login
+//Login Controller
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -47,9 +71,13 @@ const login = async (req, res) => {
       return res.status(400).json({ message: "Incorrect email or password" });
     }
 
-    res
-      .status(200)
-      .json({ user: { _id: user._id, name: user.name, email: user.email } });
+    const accessToken = createAccessToken(user);
+    const refershToken = createRefershToken(user);
+
+    res.status(200).json({
+      user: { _id: user._id, name: user.name, email: user.email },
+      token: { Access: accessToken, Refersh: refershToken },
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server Error" });
